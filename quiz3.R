225ea640bec03c6aef4435ab34b4d85e349e3d61
Chris Rodger <crodger1@gmail.com>
2015-02-23 18:00:55 -0500

    final quiz 3 file

--- quiz3.R
+++ quiz3.R
@@ -29,26 +29,39 @@ quantile(jeff,probs=c(.3,.8))
 
 
 ## question 3
-gdp<-read.csv("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv",skip=5,header=FALSE,stringsAsFactors=FALSE)
-gdp2<-gdp[1:215,]
-head(gdp2)
-tail(gdp2)
-gdp2<-cbind(Source="gdp",gdp2)
+gdp<-read.csv("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv",skip=5,nrows=190,header=FALSE,stringsAsFactors=FALSE)
+head(gdp)
+tail(gdp)
+gdp2<-cbind(Source="gdp",gdp)
 
 
 library(plyr)
-gdp2<-rename(gdp2,c("V1"="CountryCode"))
 
 ed<-read.csv("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv",stringsAsFactors=FALSE)
 head(ed[,1:4])
+tail(ed[,1:4])
 
-combine<-merge(ed,gdp2,by.x="CountryCode",by.y="CountryCode")
-head(combine)
+combine<-merge(ed,gdp2,by.x="CountryCode",by.y="V1")
+head(combine[,c("CountryCode","Short.Name","V4")])
 
-nrow(combine[combine$Source=="gdp",])
-nrow(combine[combine$V2!="",])
-names(gdp2)
-head(gdp2)
+nrow(combine)  ## number of rows in the combined data
 
 combine2<-combine[order(-combine$V2),]
-View(combine2)
+
+combine2[13,c("V2","Short.Name","V4")]
+
+library(dplyr)
+mean_data <- combine2 %>%
+  group_by(Income.Group) %>%
+  summarize(mean=mean(V2))
+
+mean_data
+
+install.packages("Hmisc")
+library(Hmisc)
+combine2$rankgroups = cut2(combine2$V2,g=5)
+combine2$rankgroups
+
+# summary(combine2)
+
+xtabs( ~ rankgroups + Income.Group, combine2)
